2adb27f3f4161a60226bbb6b065d333e99906ca3
uijin <justwaves@naver.com>
2020-06-30 10:55:28 +0900

    withError

---
+++ example/src/hoc/withError.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import withStyles, { css } from '../styles/withStyles';
+
+export default function (defaultMessage) {
+  return (WrappedComponent) => {
+    const { displayName, name: componentName } = WrappedComponent;
+    const WrappedComponentName = displayName || componentName;
+
+    function ComponentWithError({ hasError, errorMessage, styles, ...props }) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} />
+          {hasError && <div {...css(styles.error)}> {errorMessage}</div>}
+        </React.Fragment>
+      );
+    }
+
+    ComponentWithError.defaultProps = {
+      errorMessage: defaultMessage,
+    };
+
+    ComponentWithError.displayName = `withError(${WrappedComponentName})`;
+    return withStyles(({ color }) => ({
+      error: {
+        color: color.error,
+      },
+    }))(ComponentWithError);
+  };
+}
